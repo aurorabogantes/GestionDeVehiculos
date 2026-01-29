f66692543ea57fda74ac74bdcdeec5df1a2c1f26
Aurora <aurorabogantes@gmail.com>
2026-01-28 21:48:35 -0600

    Añade capa BL con servicio e interfaz para Vehiculo
    Se crea el proyecto GestionDeVehiculos.BL, se añade a la solución y se configuran referencias a DA y MO. Se implementan la interfaz IServicioVehiculo y la clase ServicioVehiculo para gestionar operaciones CRUD asíncronas sobre la entidad Vehiculo, separando la lógica de negocio del acceso a datos.

---
+++ GestionDeVehiculos.BL/IServicioVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using GestionDeVehiculos.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionDeVehiculos.BL
+{
+    internal interface IServicioVehiculo
+    {
+        Task<IEnumerable<Vehiculo>> GetAllAsync(CancellationToken ct = default);
+        Task<Vehiculo?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<Vehiculo> CreateAsync(Vehiculo vehiculo, CancellationToken ct = default);
+        Task<bool> UpdateAsync(Vehiculo vehiculo, CancellationToken ct = default);
+        Task<bool> DeleteAsync(int id, CancellationToken ct = default);
+    }
+}
---
+++ GestionDeVehiculos.BL/ServicioVehiculo.cs
@@ -0,0 +1,45 @@
+﻿using GestionDeVehiculos.DA;
+using GestionDeVehiculos.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GestionDeVehiculos.BL
+{
+    public class ServicioVehiculo : IServicioVehiculo
+    {
+        private readonly IRepositorioVehiculo _repo;
+
+        public ServicioVehiculo(IRepositorioVehiculo repo)
+        {
+            _repo = repo;
+        }
+
+        public Task<IEnumerable<Vehiculo>> GetAllAsync(CancellationToken ct = default)
+        {
+            return _repo.GettAllAsync(ct);
+        }
+
+        public Task<Vehiculo?> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            return _repo.GetByIdAsync(id, ct);
+        }
+
+        public Task<Vehiculo> CreateAsync(Vehiculo vehiculo, CancellationToken ct = default)
+        {
+            return _repo.AddAsync(vehiculo, ct);
+        }
+
+        public Task<bool> UpdateAsync(Vehiculo vehiculo, CancellationToken ct = default)
+        {
+            return _repo.UpdateAsync(vehiculo, ct);
+        }
+
+        public Task<bool> DeleteAsync(int id, CancellationToken ct = default)
+        {
+            return _repo.DeleteAsync(id, ct);
+        }
+    }
+}
