c75b7b51be288e12d260c9b6315bd3d44dc2e96c
Aurora <aurorabogantes@gmail.com>
2026-01-28 21:29:25 -0600

    Añade DA con repositorio en memoria y contrato interfaz
    Se agrega el proyecto GestionDeVehiculos.DA a la solución, incluyendo su archivo de proyecto y referencia al modelo. Se implementa la interfaz IRepositorioVehiculo con operaciones CRUD asíncronas y una clase RepositorioVehiculoEnMemoria basada en ConcurrentDictionary para gestión concurrente de vehículos en memoria.

---
+++ GestionDeVehiculos.DA/IRepositorioVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using GestionDeVehiculos.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionDeVehiculos.DA
+{
+    public interface IRepositorioVehiculo
+    {
+        Task<IEnumerable<Vehiculo>> GettAllAsync(CancellationToken ct = default);
+        Task<Vehiculo?> GetByIdAsync(int id, CancellationToken ct  = default);
+        Task<Vehiculo> AddAsync(Vehiculo vehiculo, CancellationToken ct = default);
+        Task<bool> UpdateAsync(Vehiculo vehiculo, CancellationToken ct = default);
+        Task<bool> DeleteAsync(int id, CancellationToken ct = default);
+    }
+}
---
+++ GestionDeVehiculos.DA/RepositorioVehiculoEnMemoria.cs
@@ -0,0 +1,42 @@
+﻿using GestionDeVehiculos.Model;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionDeVehiculos.DA
+{
+    public class RepositorioVehiculoEnMemoria
+    {
+        private readonly ConcurrentDictionary<int, Vehiculo> _tienda = new();
+        private int _idContador = 0;
+
+        public Task<Vehiculo> AddAsync(Vehiculo vehiculo, CancellationToken ct = default) {
+            var id = Interlocked.Increment(ref _idContador);
+            vehiculo.Id = id;
+            _tienda[id] = vehiculo;
+            return Task.FromResult(vehiculo);
+        }
+
+        public Task<bool> DeleteAsync(int id, CancellationToken ct  = default) {
+            return Task.FromResult(_tienda.TryRemove(id, out _));
+        }
+
+        public Task<IEnumerable<Vehiculo>> GetAllAsync(CancellationToken ct = default) {
+            return Task.FromResult(_tienda.Values.AsEnumerable());
+        }
+
+        public Task<Vehiculo?> GetByIdAsync(int id, CancellationToken ct = default)
+        {
+            _tienda.TryGetValue(id, out var vehiculo);
+            return Task.FromResult(vehiculo);
+        }
+
+        public Task<bool> UpdateAsync(Vehiculo vehiculo, CancellationToken ct = default)
+        {
+            if (!_tienda.ContainsKey(vehiculo.Id)) return Task.FromResult(false);
+            _tienda[vehiculo.Id] = vehiculo;
+            return Task.FromResult(true);
+        }
+    }
+}
